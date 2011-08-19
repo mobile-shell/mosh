8718869648250461f36684f933baa07333f39074
Keith Winstein <keithw@mit.edu>
2011-08-19 05:11:44 -0400

    Restore SEND_MINDELAY to 1/50 sec

--- networktransport.hpp
+++ networktransport.hpp
@@ -80,7 +80,7 @@ namespace Network {
     static const int SEND_INTERVAL_MAX = 250; /* ms between frames */
     static const int ACK_INTERVAL = 1000; /* ms between empty acks */
     static const int ACK_DELAY = 10; /* ms before delayed ack */
-    static const int SEND_MINDELAY = 50; /* ms to collect all input */
+    static const int SEND_MINDELAY = 20; /* ms to collect all input */
     static const int HEADER_LEN = 120;
 
     /* helper methods for tick() */
