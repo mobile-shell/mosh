49fc21c8a3537d661ee3607777b532e3ce5103b9
Massimo Gengarelli <gengarel@cs.unibo.it>
2012-04-19 21:33:11 +0200

    Catch by const reference (remove Eclipse warnings)
    (closes #245)

--- src/frontend/mosh-client.cc
+++ src/frontend/mosh-client.cc
@@ -124,13 +124,13 @@ int main( int argc, char *argv[] )
     }
 
     client.shutdown();
-  } catch ( Network::NetworkException e ) {
+  } catch ( const Network::NetworkException& e ) {
     fprintf( stderr, "Network exception: %s: %s\r\n",
 	     e.function.c_str(), strerror( e.the_errno ) );
-  } catch ( Crypto::CryptoException e ) {
+  } catch ( const Crypto::CryptoException& e ) {
     fprintf( stderr, "Crypto exception: %s\r\n",
 	     e.text.c_str() );
-  } catch ( std::string s ) {
+  } catch ( const std::string& s ) {
     fprintf( stderr, "Error: %s\r\n", s.c_str() );
   }
 
