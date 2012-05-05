1d0b16881cfafbb2ee9c36bd272dbb3b44f9ef5e
Keegan McAllister <mcallister.keegan@gmail.com>
2012-05-05 02:01:18 -0400

    Switch to pselect(2) and add signal handling to Select class

--- src/util/select.cc
+++ src/util/select.cc
@@ -21,3 +21,15 @@
 Select *Select::instance = NULL;
 
 fd_set Select::dummy_fd_set;
+
+sigset_t Select::dummy_sigset;
+
+void Select::handle_signal( int signum )
+{
+  fatal_assert( signum >= 0 );
+  fatal_assert( signum <= MAX_SIGNAL_NUMBER );
+
+  Select &sel = get_instance();
+  sel.got_signal[ signum ] = 1;
+  sel.got_any_signal = 1;
+}
