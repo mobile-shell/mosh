88bed3e94aee0c5f66493bbcfed6718d4e75de94
Keith Winstein <keithw@mit.edu>
2012-06-12 14:39:36 -0400

    Remove warning for IP_TOS failure

--- src/network/network.cc
+++ src/network/network.cc
@@ -118,7 +118,7 @@ void Connection::setup( void )
  /* set diffserv values to AF42 + ECT */
   uint8_t dscp = 0x92;
   if ( setsockopt( sock, IPPROTO_IP, IP_TOS, &dscp, 1) < 0 ) {
-    perror( "setsockopt( IP_TOS )" );
+    //    perror( "setsockopt( IP_TOS )" );
   }
 }
 
