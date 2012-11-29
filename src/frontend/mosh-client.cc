80a9b0fe9814d0c995350b7adee3b63dab7c2613
Anton Lundin <glance@acc.umu.se>
2012-11-29 23:35:53 +0100

    Rework curses detection
    Detect diffrent variants of curses and use them.

--- src/frontend/mosh-client.cc
+++ src/frontend/mosh-client.cc
@@ -42,7 +42,19 @@
 #include "fatal_assert.h"
 
 /* these need to be included last because of conflicting defines */
-#include <curses.h>
+#if defined HAVE_NCURSESW_CURSES_H
+#  include <ncursesw/curses.h>
+#elif defined HAVE_NCURSESW_H
+#  include <ncursesw.h>
+#elif defined HAVE_NCURSES_CURSES_H
+#  include <ncurses/curses.h>
+#elif defined HAVE_NCURSES_H
+#  include <ncurses.h>
+#elif defined HAVE_CURSES_H
+#  include <curses.h>
+#else
+#  error "SysV or X/Open-compatible Curses header file required"
+#endif
 #include <term.h>
 
 void usage( const char *argv0 ) {
