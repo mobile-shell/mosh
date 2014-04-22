fd24ce8ad0637947f19c31c95cd3597a58aab744
John Hood <cgull@glup.org>
2014-04-21 23:24:20 -0400

    use std::string instead of std::vector for Cell-- faster everywhere except RPi, alas

--- src/terminal/terminalframebuffer.h
+++ src/terminal/terminalframebuffer.h
@@ -87,7 +87,7 @@ namespace Terminal {
 
   class Cell {
   public:
-    typedef std::vector<uint8_t> content_type; /* can be std::string or std::vector<uint8_t> */
+    typedef std::string content_type; /* can be std::string, std::vector<uint8_t>, or __gnu_cxx::__vstring */
     content_type contents;
     Renditions renditions;
     uint8_t width;
