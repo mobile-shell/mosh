57bf4af1a90a42f13e57c0155fd665df9b96614d
John Hood <cgull@glup.org>
2015-06-07 04:44:21 -0400

    Allow non-inserting prediction.
    This may be useful for users who find prediction's activity right of
    the cursor distracting.
    
    Prediction underscoring is still a little weird sometimes, it replays
    a history of known/unknown changes as acks come in from the server.

--- scripts/mosh.pl
+++ scripts/mosh.pl
@@ -65,6 +65,8 @@ my $server = 'mosh-server';
 
 my $predict = undef;
 
+my $overwrite = 0;
+
 my $bind_ip = undef;
 
 my $use_remote_ip = 'proxy';
@@ -96,6 +98,8 @@ qq{Usage: $0 [options] [--] [user@]host [command...]
 -a      --predict=always        use local echo even on fast links
 -n      --predict=never         never use local echo
         --predict=experimental  aggressively echo even when incorrect
+
+-o      --overwrite             prediction overwrites instead of inserting
 
 -4      --family=inet        use IPv4 only
 -6      --family=inet6       use IPv6 only
@@ -150,6 +154,7 @@ sub predict_check {
 GetOptions( 'client=s' => \$client,
 	    'server=s' => \$server,
 	    'predict=s' => \$predict,
+	    'overwrite|o!' => \$overwrite,
 	    'port=s' => \$port_request,
 	    'a' => sub { $predict = 'always' },
 	    'n' => sub { $predict = 'never' },
@@ -199,6 +204,9 @@ if (!$have_ipv6) {
   }
   # Force IPv4.
   $family = "inet";
+}
+if ( $overwrite ) {
+    $ENV{ "MOSH_PREDICTION_OVERWRITE" } = "yes";
 }
 
 if ( defined $port_request ) {
