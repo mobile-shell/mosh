48028c289bd6526bcdef57b512d9ac57ee8b3dcf
Ashish Gupta <ashmew2@gmail.com>
2016-04-27 21:44:28 +0000

    Clarify that -p does not affect SSH port

--- scripts/mosh.pl
+++ scripts/mosh.pl
@@ -90,6 +90,7 @@ qq{Usage: $0 [options] [--] [user@]host [command...]
         --family=all         try all network types [default]
 -p PORT[:PORT2]
         --port=PORT[:PORT2]  server-side UDP port or range
+                                (No effect on server-side SSH port)
         --bind-server={ssh|any|IP}  ask the server to reply from an IP address
                                        (default: "ssh")
 
