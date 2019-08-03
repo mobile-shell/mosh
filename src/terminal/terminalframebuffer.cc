a1cca0995b1cfba86eb6cf93db86d0c7c20850f8
Stanislav Spassov <stanspas@amazon.de>
2019-08-03 19:34:43 +0200

    Add support for ECMA-48 escape sequence for crossed-out/strikethrough
    ECMA-48 defines rendition mode 9 as:
      crossed-out (characters still legible but marked as to be deleted),
    and mode 29 as the inverse:
      not crossed out
    
    Modern terminals (including xterm, tmux, libvte, etc.) support rendering
    strikethrough. Further, ncurses since version 6.1 includes the smxx/rmxx
    user-defined terminal capabilities in the tmux and xterm-basic
    databases.
    
    Note that the new attribute still fits in the eight bits allocated for
    Renditions::attributes, so there is no need to increase the size of Cell.
    
    Signed-off-by: Stanislav Spassov <stanspas@amazon.de>

--- src/terminal/terminalframebuffer.cc
+++ src/terminal/terminalframebuffer.cc
@@ -489,7 +489,7 @@ void Renditions::set_rendition( color_type num )
     return;
   }
 
-  bool value = num < 9;
+  bool value = num < 10;
   switch ( num ) {
   case 1: set_attribute(bold, value); break;
   case 2: set_attribute(faint, value); break;
@@ -502,6 +502,7 @@ void Renditions::set_rendition( color_type num )
   case 5: case 25: set_attribute(blink, value); break;
   case 7: case 27: set_attribute(inverse, value); break;
   case 8: case 28: set_attribute(invisible, value); break;
+  case 9: case 29: set_attribute(strikethrough, value); break;
   default: break; /* ignore unknown rendition */
   }
 }
@@ -537,6 +538,7 @@ std::string Renditions::sgr( void ) const
   if ( get_attribute( blink ) ) ret.append( ";5" );
   if ( get_attribute( inverse ) ) ret.append( ";7" );
   if ( get_attribute( invisible ) ) ret.append( ";8" );
+  if ( get_attribute( strikethrough ) ) ret.append( ";9" );
 
   if ( foreground_color ) {
     if ( is_true_color( foreground_color ) ) {
