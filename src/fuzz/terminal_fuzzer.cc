adb62e97ffe10d00be1f6fa0744d568cbda631c9
Alex Chernyakhovsky <alex@achernya.com>
2022-05-30 20:01:52 -0400

    Add fuzzer for the terminal
    This commit adds a fuzzer for more of the terminal pipeline, adding
    coverage for the input and output portions of the terminal
    framebuffer.

---
+++ src/fuzz/terminal_fuzzer.cc
@@ -0,0 +1,17 @@
+#include <cstddef>
+#include <cstdint>
+
+#include "parser.h"
+#include "completeterminal.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  Terminal::Display display(false);
+  Terminal::Complete complete(80, 24);
+  Terminal::Framebuffer state(80, 24);
+  for (size_t i = 0; i < size; i++) {
+    complete.act(Parser::UserByte(data[i]));
+  }
+  display.new_frame(true, state, complete.get_fb());
+  
+  return 0;
+}
