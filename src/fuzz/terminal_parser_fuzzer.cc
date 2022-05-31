0c6e034459610ca3abef650512a0b5c8ab552799
Alex Chernyakhovsky <alex@achernya.com>
2022-05-30 20:00:04 -0400

    Add fuzzing infrastructure
    This commit adds the --enable-fuzzing (and --enable-asan, to make
    fuzzing more useful) options and a sample fuzzer for the terminal
    parser. At this time only libfuzzer is supported. Future changes to
    add AFL to get more fuzzing capability should be possible with the
    addition of the afl_driver.cc from Chromium.

---
+++ src/fuzz/terminal_parser_fuzzer.cc
@@ -0,0 +1,15 @@
+#include <cstddef>
+#include <cstdint>
+
+#include "parser.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  Parser::UTF8Parser parser;
+  Parser::Actions result;
+
+  for (size_t i = 0; i < size; i++) {
+    parser.input(data[i], result);
+  }
+  
+  return 0;
+}
