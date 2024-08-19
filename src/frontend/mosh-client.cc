80b680fade131c891934c52275d8a7fc10fc030e
Mike Frysinger <vapier@chromium.org>
2024-08-19 11:09:38 -0400

    update https URIs in more places
    This doesn't update every URI, just the ones generally user facing.

--- src/frontend/mosh-client.cc
+++ src/frontend/mosh-client.cc
@@ -75,7 +75,7 @@ static void print_version( FILE* file )
 {
   fputs( "mosh-client (" PACKAGE_STRING ") [build " BUILD_VERSION "]\n"
          "Copyright 2012 Keith Winstein <mosh-devel@mit.edu>\n"
-         "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n"
+         "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n"
          "This is free software: you are free to change and redistribute it.\n"
          "There is NO WARRANTY, to the extent permitted by law.\n",
          file );
